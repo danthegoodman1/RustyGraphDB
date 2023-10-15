6050ac27d90d1c206c8a8660b9d99ae074a22914
Dan Goodman <xxdanthegoodmanxx@gmail.com>
2023-10-15 12:50:41 -0400

    traversal time test

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 pub mod graph;
 pub mod node;
 
+use std::time::Instant;
+
 use graph::Graph;
 use node::Node;
 
@@ -11,8 +13,10 @@ fn main() {
     let b_node = graph.add_node(Node::new("b"));
     let c_node = graph.add_node(Node::new("c"));
 
+    // Make demo circular relation
     graph.add_relation(&a_node, &b_node, "friends");
     graph.add_relation(&b_node, &c_node, "friends");
+    graph.add_relation(&c_node, &a_node, "friends");
 
     // Test getting a node in block so lock releases
     let node_a = graph
@@ -62,4 +66,38 @@ fn main() {
                 .id
         );
     }
+
+    // Traverse 10M times
+    let start = Instant::now();
+    let mut current = a_node.clone();
+
+    for _ in 0..10_000_000 {
+        // scope because current is borroed
+        let next_node = {
+            let relation = &current.read().expect("failed to read").neighbors[0];
+            let direction = relation.any_direction();
+            direction.upgrade().expect("failed to upgrade!")
+        };
+        current = next_node;
+    }
+
+    let end = start.elapsed().as_millis();
+    println!("Traversed 10M in {}ms", end);
+    // debug: Traversed 10M in 1149ms --- release: Traversed 10M in 334ms
+
+
+    // let mut i = 0;
+    // while start.elapsed().as_millis() < 1000 {
+    //     // scope because current is borroed
+    //     let next_node = {
+    //         let relation = &current.read().expect("failed to read").neighbors[0];
+    //         let direction = relation.any_direction();
+    //         direction.upgrade().expect("failed to upgrade!")
+    //     };
+    //     current = next_node;
+    //     i+=1;
+    // }
+    // println!("Traversed {} in 1 second", i);
+    // release: Traversed 14584449 in 1 second
+    // I think we are spending half the time incrementing
 }
